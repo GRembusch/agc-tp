5fa0cdd7869f3aaf4911b8d3a7252a6a790e96f7
Gabrielle Rembusch <gabrielle.rembusch@gmail.com>
2021-01-04 15:41:33 +0100

    Dé-duplication en séquence "complète"

--- agc/agc.py
+++ agc/agc.py
@@ -18,7 +18,7 @@ import sys
 import os
 import gzip
 import statistics
-from collections import Counter
+from collections import Counter, defaultdict
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
@@ -55,7 +55,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True,
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
     parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
                         help="Minimum sequence length for dereplication")
@@ -69,12 +69,45 @@ def get_arguments():
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
 
+
 def read_fasta(amplicon_file, minseqlen):
-    pass
+    """Generator that reads the given file.
+      :Parameters:
+          amplicon_file: fasta.gz file to read
+          minseqlen: Minimal sequence length (integer)
+    """
+    myfile = gzip.open(amplicon_file)
+    for line in myfile: # >Name
+        sequence = ''
+        current_line = str(line)[2:-3]
+        if current_line == '':
+            break
+        if current_line[0] == '>':
+            current_line = str(next(myfile))[2:-3]
+        while current_line != '' and current_line[0] != '>':
+            sequence += current_line
+            current_line = str(next(myfile))[2:-3]
+        if len(sequence) >= minseqlen:
+            yield sequence
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
-    pass
+    """Generator of sequences for which the number of occurences is higher than mincount
+      :Parameters:
+          amplicon_file: fasta.gz file to read
+          minseqlen: Minimal sequence length (integer)
+          mincount: minimum number of sequences (integer)
+    """
+    seq_dict = defaultdict(lambda: 0)
+    for sequence in read_fasta(amplicon_file,minseqlen):
+        seq_dict[sequence] += 1
+    sorted_occurences = sorted(seq_dict.items(), key = lambda kv: kv[1], reverse=True)
+    sorted_dict = dict(sorted_occurences)
+    print(sorted_dict)
+    for key in sorted_dict:
+        if seq_dict[key] >= mincount:
+            yield key, seq_dict[key]
+
 
 
 def get_chunks(sequence, chunk_size):
@@ -84,7 +117,7 @@ def get_unique(ids):
     return {}.fromkeys(ids).keys()
 
 
-def common(lst1, lst2): 
+def common(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 def cut_kmer(sequence, kmer_size):
@@ -114,6 +147,8 @@ def main():
     """
     # Get arguments
     args = get_arguments()
+    for [key,value] in dereplication_fulllength(args.amplicon_file,200,3):
+        print(key)
 
 
 if __name__ == '__main__':
