070ee6b57a6c3121850cbaf879ae5eb9ee660dfb
Gabrielle Rembusch <gabrielle.rembusch@gmail.com>
2021-01-10 14:50:24 +0100

    Recherche de séquences chimériques par approche "de novo"

--- agc/agc.py
+++ agc/agc.py
@@ -108,10 +108,19 @@ def dereplication_fulllength(amplicon_file, minseqlen, mincount):
         if seq_dict[key] >= mincount:
             yield key, seq_dict[key]
 
-
-
 def get_chunks(sequence, chunk_size):
-    pass
+    """Takes a sequence and returns chunks of given size.
+      :Parameters:
+          sequence: The sequence to cut.
+          chunk_size: Size of a chunk.
+    """
+    chunks = []
+    for i in range(0,len(sequence)-chunk_size,chunk_size):
+        chunks.append(sequence[i:i+chunk_size])
+    if len(chunks) >= 4:
+        return chunks
+    else:
+        return None
 
 def get_unique(ids):
     return {}.fromkeys(ids).keys()
@@ -121,13 +130,126 @@ def common(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 def cut_kmer(sequence, kmer_size):
-    pass
+    """Generator that extracts kmers from a sequence.
+      :Parameters:
+          sequence: The sequence to cut.
+          kmer_size: Size of a kmer
+    """
+    for i in range(0, len(sequence) - kmer_size +1): # remove the "\n" at the end
+        yield sequence[i:i+kmer_size]
+
+
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    """Add the kmers from the given sequence to the given kmer dictionary, with the sequence id.
+      :Parameters:
+          kmer_dict: Dictionary of kmers.
+          sequence: The sequence we add to the dict.
+          id_seq : Id of the sequence.
+          kmer_size: The size of the kmers.
+    """
+    for kmer in cut_kmer(sequence, kmer_size):
+        if kmer in kmer_dict:
+            kmer_dict[kmer].append(id_seq)
+        else:
+            kmer_dict[kmer] = [id_seq]
+    return kmer_dict
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    """Get the 8 sequences from the given kmer dict that are most similar to the given sequence.
+      :Parameters:
+          kmer_dict: Dictionary of kmers.
+          sequence: The sequence we compare to the dict.
+          kmer_size: The size of the kmers.
+    """
+    counter = Counter([ids for kmer in cut_kmer(sequence, kmer_size)
+        if kmer in kmer_dict for ids in kmer_dict[kmer]])
+    mates = [mate[0] for mate in counter.most_common(8)]
+    return mates
 
 def get_identity(alignment_list):
-    pass
+    """Get the identity score of 2 sequences.
+      :Parameters:
+          alignment_list: List containing the 2 sequences to compare.
+    """
+    seq1 = alignment_list[0]
+    seq2 = alignment_list[1]
+    nb_identical = 0
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            nb_identical += 1
+    return (nb_identical / len(seq1) * 100)
+
+def detect_chimera(perc_identity_matrix):
+    """Determines if a sequence is a chimera or not.
+      :Parameters:
+          perc_identity_matrix: Matrix containing the identity rate between sequences.
+    """
+    stddev = 0
+    seq1_sim = []
+    seq2_sim = []
+    for perc_identity in perc_identity_matrix:
+        stddev += statistics.stdev(perc_identity)
+        perc1 = perc_identity[0]
+        perc2 = perc_identity[1]
+        if perc1 not in seq1_sim:
+            seq1_sim.append(perc1)
+        if perc2 not in seq2_sim:
+            seq2_sim.append(perc2)
+    if len(seq1_sim) >= 2 or len(seq2_sim) >= 2:
+        average_stddev = stddev / len(perc_identity_matrix)
+        if average_stddev > 5:
+            return True
+    return False
+
+def get_identity_matrix(chunks, parents, sequence_bank, chunk_size):
+    """Get the identity matrix between a sequence and 2 parents.
+      :Parameters:
+          chunks: Chunks from the candidate sequence.
+          parents: Parent sequences from the candidate sequence.
+          sequence_bank: List of sequences that are not chimeras.
+          chunk_size:  Size of the chunks.
+    """
+    perc_identity_matrix = [[] for chunk_index in range(len(chunks))]
+    for parent in parents:
+        parent_chunks = get_chunks(sequence_bank[parent], chunk_size)
+        for index, chunk in enumerate(chunks):
+            alignment = nw.global_align(chunk, parent_chunks[index])
+            identity = get_identity(alignment)
+            perc_identity_matrix[index].append(identity)
+    return perc_identity_matrix
 
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    """Generator of non chimeric sequences.
+      :Parameters:
+          amplicon_file: fasta.gz file to read.
+          minseqlen: Minimal sequence length (integer).
+          mincount: minimum number of sequences (integer).
+          chunk_size: Size of chunks.
+          kmer_size: size of kmers.
+    """
+    sequence_generator = dereplication_fulllength(amplicon_file,minseqlen,mincount)
+    kmer_dict = {}
+    sequence_bank = []
+    id_sequence = 0
+    perc_identity_matrix = []
+
+    for sequence, index in sequence_generator:
+        chunks = get_chunks(sequence, chunk_size)[:4]
+        mates = []
+        parents = []
+        for chunk in chunks:
+            mate = search_mates(kmer_dict,chunk,kmer_size)
+            if mate not in mates:
+                mates.append(mate)
+                parents = common(parents,mate)
+        if len(parents) >= 2:
+            perc_identity_matrix = get_identity_matrix(chunks, parents[:2],
+                chunk_size, sequence_bank)
+        if not detect_chimera(perc_identity_matrix):# try ! instead of not
+            kmer_dict = get_unique_kmer(kmer_dict,sequence,id_sequence, kmer_size)
+            sequence_bank.append(sequence)
+            id_sequence +=1
+            yield [sequence, index]
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
     pass
